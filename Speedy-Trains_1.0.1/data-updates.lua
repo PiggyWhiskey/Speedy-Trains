a53da81d615fc6ed72d2a5de95c79793239240e0
PiggyWhiskey <bromy2004@gmail.com>
2019-04-05 17:27:31 +0930

    Angels Smelting/Petro Trains compat

---
+++ Speedy-Trains_1.0.1/data-updates.lua
@@ -0,0 +1,84 @@
+require("functions")
+---------------------------------------------------------
+--LOCOMOTIVES+WAGONS
+---------------------------------------------------------
+
+--updateLocomotive ( entity, weight, max_speed, max_power, reversing_power_modifier, braking_force, friction_force, air_resistance, max_health )
+--updateCargoWagon ( entity, weight, max_speed, braking_force, friction_force, air_resistance, max_health, inventory_size )
+
+
+local tmp = data.raw["locomotive"]["locomotive"]
+updateLocomotive(tmp, nil, 4.00, "2400kW", nil, nil, nil, nil, nil)
+
+local tmp = data.raw["cargo-wagon"]["cargo-wagon"]
+updateCargoWagon(tmp, nil, 4.00, nil, nil, nil, nil, nil)
+
+
+if mods["boblogistics"] then
+	----------------------------
+	local tmp = data.raw["locomotive"]["bob-locomotive-2"]
+	if tmp then
+		updateLocomotive(tmp, nil, 8.00, "3600kW", nil, nil, nil, nil, nil)
+	end
+
+	local tmp = data.raw["locomotive"]["bob-locomotive-3"]
+	if tmp then
+		updateLocomotive(tmp, nil, 16.00, "7200kW", nil, nil, nil, nil, nil)
+	end
+	----------------------------
+
+	----------------------------
+	local tmp = data.raw["cargo-wagon"]["bob-cargo-wagon-2"]
+	if tmp then
+		updateCargoWagon(tmp, nil, 8.00, nil, nil, nil, nil, nil)
+	end
+	
+	local tmp = data.raw["cargo-wagon"]["bob-cargo-wagon-3"]
+	if tmp then
+		updateCargoWagon(tmp, nil, 16.00, nil, nil, nil, nil, nil)
+	end
+	----------------------------
+end
+
+if mods["angelsaddons-petrotrain"] then
+	----------------------------
+	local tmp = data.raw["locomotive"]["petro-locomotive-1"]
+	if tmp then
+		updateLocomotive(tmp, nil, 8.00, "3600kW", nil, nil, nil, nil, nil)
+	end
+	----------------------------
+	
+	----------------------------
+	local tmp = data.raw["fluid-wagon"]["petro-tank1"]
+	if tmp then
+		updateFluidWagon(tmp, nil, 8.00, nil, nil, nil, nil, nil)
+	end
+
+	local tmp = data.raw["fluid-wagon"]["petro-tank2"]
+	if tmp then
+		updateFluidWagon(tmp, nil, 8.00, nil, nil, nil, nil, nil)
+	end
+	----------------------------
+end
+
+if mods["angelsaddons-smeltingtrain"] then
+	----------------------------
+	local tmp = data.raw["locomotive"]["smelting-locomotive-1"]
+	if tmp then
+		updateLocomotive(tmp, nil, 8.00, "3600kW", nil, nil, nil, nil, nil)
+	end
+
+	local tmp = data.raw["locomotive"]["smelting-locomotive-tender"]
+	if tmp then
+		updateLocomotive(tmp, nil, 8.00, "3600kW", nil, nil, nil, nil, nil)
+	end
+	----------------------------
+	
+	----------------------------
+	local tmp = data.raw["cargo-wagon"]["smelting-wagon-1"]
+	if tmp then
+		updateCargoWagon(tmp, nil, 8.00, nil, nil, nil, nil, nil)
+	end
+	----------------------------
+end
+
---
+++ Speedy-Trains_1.0.1/functions.lua
@@ -0,0 +1,31 @@
+function updateLocomotive ( entity, weight, max_speed, max_power, reversing_power_modifier, braking_force, friction_force, air_resistance, max_health )
+	entity.weight = weight or entity.weight
+	entity.max_speed = max_speed or entity.max_speed
+	entity.max_power = max_power or entity.max_power
+	entity.reversing_power_modifier = reversing_power_modifier or entity.reversing_power_modifier
+	entity.braking_force = braking_force or entity.braking_force
+	entity.friction_force = friction_force or entity.friction_force
+	entity.air_resistance = air_resistance or entity.air_resistance
+	entity.max_health = max_health or entity.max_health
+end
+
+
+function updateCargoWagon ( entity, weight, max_speed, braking_force, friction_force, air_resistance, max_health, inventory_size )
+	entity.weight = weight or entity.weight
+	entity.max_speed = max_speed or entity.max_speed
+	entity.braking_force = braking_force or entity.braking_force
+	entity.friction_force = friction_force or entity.friction_force
+	entity.air_resistance = air_resistance or entity.air_resistance
+	entity.max_health = max_health or entity.max_health
+	entity.inventory_size = inventory_size or entity.inventory_size
+end
+
+function updateFluidWagon ( entity, weight, max_speed, braking_force, friction_force, air_resistance, max_health, capacity )
+	entity.weight = weight or entity.weight
+	entity.max_speed = max_speed or entity.max_speed
+	entity.braking_force = braking_force or entity.braking_force
+	entity.friction_force = friction_force or entity.friction_force
+	entity.air_resistance = air_resistance or entity.air_resistance
+	entity.max_health = max_health or entity.max_health
+	entity.inventory_size = inventory_size or entity.inventory_size
+end
